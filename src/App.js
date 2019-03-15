146ee04f61e019abda9e90faa994f1a123061e1c
jddelia <jddelia123@gmail.com>
2019-03-15 08:01:41 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Onboarder from './components/Onboarder';
 class App extends Component {
   render() {
     return (
-      <div className="">
+      <div>
         <Onboarder />
       </div>
     );
